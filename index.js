f069796dcb9cb0f2d243340fb369dddee41e44aa
nirav7707 <56816580+nirav7707@users.noreply.github.com>
2021-01-01 12:14:04 +0530

    cors is added

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const express = require("express")
 const app = express()
 
 require('./startup/logging')();
+require('./startup/cors')(app);
 require('./startup/routes')(app);
 require('./startup/db')();
 require('./startup/config')();
