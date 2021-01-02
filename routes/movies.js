88893ad4e02233b1c36d91d78ba6df3c4ca666de
nirav7707 <56816580+nirav7707@users.noreply.github.com>
2021-01-02 11:27:40 +0530

    movie route put delete ang getMovie method added

--- routes/movies.js
+++ routes/movies.js
@@ -4,7 +4,8 @@ const mongoose =  require('mongoose');
 const express = require("express")
 const auth = require('../middleware/auth')
 const router = express.Router();
-const validate =require('../middleware/validate')
+const validate =require('../middleware/validate');
+const admin = require('../middleware/admin');
 router.get('/', async (req,res) =>{
     const movie = await Movie.find();
     res.send(movie)
@@ -31,6 +32,52 @@ movie = await movie.save();
 
 res.send(movie)
 
-})
+});
+
+
+router.put("/:id", [auth], async (req, res) => {
+    const { error } = validate(req.body);
+    if (error) return res.status(400).send(error.details[0].message);
+  
+    const genre = await Genre.findById(req.body.genreId);
+    if (!genre) return res.status(400).send("Invalid genre.");
+  
+    const movie = await Movie.findByIdAndUpdate(
+      req.params.id,
+      {
+        title: req.body.title,
+        genre: {
+          _id: genre._id,
+          name: genre.name
+        },
+        numberInStock: req.body.numberInStock,
+        dailyRentalRate: req.body.dailyRentalRate
+      },
+      { new: true }
+    );
+  
+    if (!movie)
+      return res.status(404).send("The movie with the given ID was not found.");
+  
+    res.send(movie);
+  });
+  
+  router.delete("/:id", [auth, admin], async (req, res) => {
+    const movie = await Movie.findByIdAndRemove(req.params.id);
+  
+    if (!movie)
+      return res.status(404).send("The movie with the given ID was not found.");
+  
+    res.send(movie);
+  });
+  
+  router.get("/:id", validateObjectId , async (req, res) => {
+    const movie = await Movie.findById(req.params.id).select("-__v");
+  
+    if (!movie)
+      return res.status(404).send("The movie with the given ID was not found.");
+  
+    res.send(movie);
+  });
 
 module.exports = router;
